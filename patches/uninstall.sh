67ba82a2cf8326b7d42fd0a618ee62913904e9cf
Dargons10 <jdamr2p@gmail.com>
2020-04-20 16:21:44 +0200

    update patches for Oreo

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="system/core frameworks/native external/selinux"
+dirs="system/core frameworks/native external/selinux bionic/libm"
 
 for dir in $dirs ; do
 	cd $rootdirectory
